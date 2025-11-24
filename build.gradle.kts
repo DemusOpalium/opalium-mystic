f265cb0b77e003899151f7e56f95c43c7128fb77
DemusOpalium <majordemus@yahoo.de>
2025-11-24 19:45:18 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,16 @@ plugins {
 group = "de.opalium"
 version = "1.0.0"
 
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    // NEU: VaultAPI-Repo
+    maven("https://jitpack.io")
+    // Alternative (falls du lieber minebench nutzt):
+    // maven("https://repo.minebench.de/")
+}
+
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
@@ -15,12 +25,6 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
-}
-
-repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 tasks.withType<JavaCompile>().configureEach {
