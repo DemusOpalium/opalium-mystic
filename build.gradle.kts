151fe96d660caa097ae5339441687a7ba79d44f3
DemusOpalium <majordemus@yahoo.de>
2025-11-25 19:51:02 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,17 +7,29 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+
+    // Paper API
     maven("https://repo.papermc.io/repository/maven-public/")
+
+    // PlaceholderAPI
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-    // NEU: VaultAPI-Repo
+
+    // VaultAPI über JitPack
     maven("https://jitpack.io")
-    // Alternative (falls du lieber minebench nutzt):
-    // maven("https://repo.minebench.de/")
 }
 
 dependencies {
+    // Paper-API für 1.21.10
     compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
+
+    // Vault-API (funktionierende Koordinate, nicht 1.7.1)
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
+        // Bukkit/Spigot nicht mitziehen, da vom Server bereitgestellt
+        exclude(group = "org.bukkit", module = "bukkit")
+        exclude(group = "org.spigotmc", module = "spigot-api")
+    }
+
+    // PlaceholderAPI
     compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
