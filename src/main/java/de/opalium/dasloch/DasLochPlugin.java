50974f80807fa0859a5f2f34015844404a1856f6
DemusOpalium <majordemus@yahoo.de>
2025-11-25 01:09:30 +0100

    Update DasLochPlugin.java

--- src/main/java/de/opalium/dasloch/DasLochPlugin.java
+++ src/main/java/de/opalium/dasloch/DasLochPlugin.java
@@ -56,86 +56,46 @@ public class DasLochPlugin extends JavaPlugin {
         }
     }
 
-    private void registerCommands() {
-        MysticWellCommand wellCommand = new MysticWellCommand(itemService, mysticWellService, vaultService);
+private void registerCommands(
+        ItemsConfig itemsConfig,
+        LifeTokenService lifeTokenService,
+        de.opalium.dasloch.service.ItemFactory itemFactory,
+        de.opalium.dasloch.service.MysticWellService mysticWellService,
+        de.opalium.dasloch.integration.VaultService vaultService
+) {
+    // Zentrale Well-Command-Instanz (für /mysticwell und /dasloch well …)
+    MysticWellCommand wellCommand = new MysticWellCommand(
+            itemsConfig,
+            lifeTokenService,
+            itemFactory,
+            mysticWellService,
+            vaultService
+    );
 
-        PluginCommand legend = getCommand("legendgive");
-        if (legend != null) {
-            LegendGiveCommand executor = new LegendGiveCommand(itemService);
-            legend.setExecutor(executor);
-            legend.setTabCompleter(executor);
-        }
-        PluginCommand mystic = getCommand("mysticgive");
-        if (mystic != null) {
-            MysticGiveCommand executor = new MysticGiveCommand(itemService);
-            mystic.setExecutor(executor);
-            mystic.setTabCompleter(executor);
-        }
-        PluginCommand mysticWell = getCommand("mysticwell");
-        if (mysticWell != null) {
-            mysticWell.setExecutor(wellCommand);
-            mysticWell.setTabCompleter(wellCommand);
-        }
-        PluginCommand dasloch = getCommand("dasloch");
-        if (dasloch != null) {
-            dasloch.setExecutor(new DasLochCommand(this, itemService, wellCommand));
-        }
+    // /legendgive
+    var legend = getCommand("legendgive");
+    if (legend != null) {
+        LegendGiveCommand executor = new LegendGiveCommand(itemsConfig, itemFactory);
+        legend.setExecutor(executor);
     }
 
-    private void registerListeners() {
-        PluginManager plugins = getServer().getPluginManager();
-        plugins.registerEvents(new CombatListener(this), this);
+    // /mysticgive
+    var mystic = getCommand("mysticgive");
+    if (mystic != null) {
+        MysticGiveCommand executor = new MysticGiveCommand(itemsConfig, itemFactory);
+        mystic.setExecutor(executor);
     }
 
-    private void registerPlaceholderApi() {
-        if (getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
-            new PlaceholderHook(this).register();
-        }
+    // /dasloch
+    var dasloch = getCommand("dasloch");
+    if (dasloch != null) {
+        dasloch.setExecutor(new DasLochCommand(this, itemsConfig, lifeTokenService, wellCommand));
     }
 
-    private void reloadServices() throws IOException {
-        YamlConfiguration itemConfig = loadConfig("items.yml");
-        YamlConfiguration enchantConfig = loadConfig("enchants.yml");
-        YamlConfiguration wellConfig = loadConfig("well.yml");
-
-        this.enchantRegistry = new EnchantRegistry(enchantConfig);
-        this.mysticWellService = new MysticWellService(wellConfig);
-        this.itemService = new MysticItemService(this, itemConfig, enchantRegistry, mysticWellService);
-    }
-
-    private YamlConfiguration loadConfig(String name) throws IOException {
-        File file = new File(getDataFolder(), name);
-        if (!file.exists()) {
-            saveResource(name, false);
-        }
-        return YamlConfiguration.loadConfiguration(file);
-    }
-
-    public EnchantsConfig getEnchantsConfig() {
-        return enchantsConfig;
-    }
-
-    public WellConfig getWellConfig() {
-        return wellConfig;
-    }
-
-    public EnchantParser getEnchantParser() {
-        return enchantParser;
-    }
-
-    public MysticWellService getMysticWellService() {
-        return mysticWellService;
-    }
-
-    public EnchantRegistry getEnchantRegistry() {
-        return enchantRegistry;
-    }
-
-    public MysticItemService getItemService() {
-        return itemService;
-    }
-
-    public VaultService getVaultService() {
-        return vaultService;
+    // /mysticwell (Direkter Zugriff auf den Brunnen)
+    var well = getCommand("mysticwell");
+    if (well != null) {
+        well.setExecutor(wellCommand);
+        well.setTabCompleter(wellCommand);
     }
 }
